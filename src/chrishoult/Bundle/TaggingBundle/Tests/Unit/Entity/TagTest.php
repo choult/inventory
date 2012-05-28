6f676db11e58d8cb6bb2cf5ca518ccd3e42079b8
Christopher Hoult <choult@gmail.com>
2012-05-28 23:46:05 +0100

    Added @covers tags

--- src/chrishoult/Bundle/TaggingBundle/Tests/Unit/Entity/TagTest.php
+++ src/chrishoult/Bundle/TaggingBundle/Tests/Unit/Entity/TagTest.php
@@ -5,14 +5,25 @@ namespace chrishoult\Bundle\TaggingBundle\Tests\Unit\Entity;
 use \chrishoult\Bundle\TaggingBundle\Entity\Tag;
 use \chrishoult\Tests\TestCase;
 
+/**
+ * @covers \chrishoult\Bundle\TaggingBundle\Entity\Tag
+ */
 class TagTest extends TestCase
 {
 
+    /**
+     * {@inheritdoc}
+     * @return \chrishoult\Bundle\TaggingBundle\Entity\Tag
+     */
     public function getObject()
     {
         return new Tag();
     }
 
+    /**
+     * {@inheritdoc}
+     * @return array
+     */
     public function getterSetterProvider()
     {
         return array(
